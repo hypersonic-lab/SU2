0ebc0a1df7e7431a4ec696314ae808a8f0f38ac1
Aaron Larsen <aaronml@math.arizona.edu>
2025-04-03 15:47:27 -0700

    update for debugging

--- run_build.sh
+++ run_build.sh
@@ -1,2 +1,2 @@
-./meson.py build -Denable-mpp=true --prefix=$PWD/SU2_Install --reconfigure
+./meson.py build -Denable-mpp=true --prefix=$PWD/install --reconfigure -Ddebug=true --optimization=0 -Dwith-mpi=disabled
 ./ninja -C build install
