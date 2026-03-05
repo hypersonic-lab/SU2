b26b0941803f02e265fe837ee5be7a6c2a190c27
will3807 <will3807@purdue.edu>
2026-03-05 15:50:34 -0500

    charge_density = M to check unit 4

--- SU2_CFD/src/variables/CNEMOEulerVariable.cpp
+++ SU2_CFD/src/variables/CNEMOEulerVariable.cpp
@@ -286,7 +286,7 @@ bool CNEMOEulerVariable::Cons2PrimVar(su2double *U, su2double *V,
   //   num_density = rhos[iSpecies] / (M[iSpecies] / 1000); // Check kg vs g
   //   charge_density += num_density * Cs[iSpecies];
   // }
-  charge_density = M[1];
+  charge_density = 10;
   V[CHARGE_INDEX] = charge_density;
 
   return nonPhys;
